26857e5b95b8cef89c541e363ac8bda9e74b2ae1
alexander480 <alexander480@gmail.com>
2019-03-26 20:18:29 -0400

    Initial Commit

---
+++ Event Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Event Countdown
+//
+//  Created by Delta Vel on 3/26/19.
+//  Copyright © 2019 Delta Vel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
